3da17a50aa49f432c96d848139fa66880a2e1692
Sachin <sachin.kharabanda@aol.com>
2025-11-24 11:35:22 +0000

    first commit

---
+++ src/main/java/me/sachin/rTP_Tweaks/RTP_Tweaks.java
@@ -0,0 +1,17 @@
+package me.sachin.rTP_Tweaks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RTP_Tweaks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
